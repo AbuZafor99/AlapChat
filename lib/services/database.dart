e7bd884e4564e67ee5e82c19803059b5b1afe589
zaforsadiq987@gmail.com <138440583+AbuZafor99@users.noreply.github.com>
2025-08-01 15:20:47 +0600

    add functionality in chat page

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -37,4 +37,23 @@ class DatabaseMethods {
         .where("SearchKey", isEqualTo: username.substring(0, 1).toUpperCase())
         .get();
   }
+
+  createChatRoom(
+    String chatRoomId,
+    Map<String, dynamic> chatRoomInfoMap,
+  ) async {
+    final snapshot = await FirebaseFirestore.instance
+        .collection("Chatrooms")
+        .doc(chatRoomId)
+        .get();
+
+    if (snapshot.exists) {
+      return true;
+    } else {
+      return FirebaseFirestore.instance
+          .collection("chatrooms")
+          .doc(chatRoomId)
+          .set(chatRoomInfoMap);
+    }
+  }
 }
--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -14,41 +14,58 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  TextEditingController searchTEController=TextEditingController();
-  bool search=false;
+  String? myUserName, myName, myEmail, myPicture;
+  TextEditingController searchTEController = TextEditingController();
+  bool search = false;
 
-  var quaryResultSet=[];
-  var tempSearchStore=[];
+  getDataFromSharedPref() async {
+    myUserName = await SharedPreferenceHelper().getUserName();
+    myName = await SharedPreferenceHelper().getUserDisplayName();
+    myEmail = await SharedPreferenceHelper().getUserEmail();
+    myPicture = await SharedPreferenceHelper().getUserImage();
 
-  getChatRoomIdByUserName(String a, String b){
-    if(a.substring(0,1).codeUnitAt(0)>b.substring(0,1).codeUnitAt(0)){
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    getDataFromSharedPref();
+    super.initState();
+  }
+
+  var quaryResultSet = [];
+  var tempSearchStore = [];
+
+  getChatRoomIdByUserName(String a, String b) {
+    if (a.substring(0, 1).codeUnitAt(0) > b.substring(0, 1).codeUnitAt(0)) {
       return "$b\_$a";
-    }else{
+    } else {
       return "$a\_$b";
     }
   }
-  initialSearch(value){
-    if(value.length == 0){
+
+  initialSearch(value) {
+    if (value.length == 0) {
       setState(() {
-          quaryResultSet=[];
-          tempSearchStore=[];
-        }
-      );
+        quaryResultSet = [];
+        tempSearchStore = [];
+      });
       setState(() {
-        search=true;
+        search = true;
       });
 
-      var capitalizedValue=value.substring(0,1).toUpperCase()+value.substring(1);
-      if(quaryResultSet.isEmpty && value.length ==1){
-        DatabaseMethods().Search(value).then((QuerySnapshot docs){
-          for (int i=0; i< docs.docs.length; i++){
+      var capitalizedValue =
+          value.substring(0, 1).toUpperCase() + value.substring(1);
+      if (quaryResultSet.isEmpty && value.length == 1) {
+        DatabaseMethods().Search(value).then((QuerySnapshot docs) {
+          for (int i = 0; i < docs.docs.length; i++) {
             quaryResultSet.add(docs.docs[i].data());
           }
         });
-      }else{
-        tempSearchStore=[];
-        quaryResultSet.forEach((element){
-          if(element['username'].startsWith(capitalizedValue)){
+      } else {
+        tempSearchStore = [];
+        quaryResultSet.forEach((element) {
+          if (element['username'].startsWith(capitalizedValue)) {
             setState(() {
               tempSearchStore.add(element);
             });
@@ -97,8 +114,17 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
                 Spacer(),
                 GestureDetector(
-                  onTap: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>ChatScreen(name: "name", profileUrl: "profileUrl", userName: "userName")));
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ChatScreen(
+                          name: "name",
+                          profileUrl: "profileUrl",
+                          userName: "userName",
+                        ),
+                      ),
+                    );
                   },
                   child: Container(
                     margin: EdgeInsets.only(right: 15),
@@ -106,7 +132,11 @@ class _HomeScreenState extends State<HomeScreen> {
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(15),
                     ),
-                    child: Icon(Icons.person, color: Color(0xff703eff), size: 30),
+                    child: Icon(
+                      Icons.person,
+                      color: Color(0xff703eff),
+                      size: 30,
+                    ),
                   ),
                 ),
               ],
@@ -155,34 +185,35 @@ class _HomeScreenState extends State<HomeScreen> {
                     const SizedBox(height: 30),
                     search
                         ? ListView(
-                            padding: EdgeInsets.only(left: 10,right: 10),
+                            padding: EdgeInsets.only(left: 10, right: 10),
                             primary: false,
                             shrinkWrap: true,
-                            children: tempSearchStore.map((element){
+                            children: tempSearchStore.map((element) {
                               return buildResultCard(element);
-                            }).toList())
-                        :Material(
-                      elevation: 1,
-                      shadowColor: Color(0xffb8b7ce),
-                      borderRadius: BorderRadius.circular(10),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Color(0xffececf8),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: TextField(
-                          controller: searchTEController,
-                          onChanged: (value){
-                            initialSearch(value.toUpperCase());
-                          },
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            prefixIcon: Icon(Icons.search),
-                            hint: Text("Search Username..."),
+                            }).toList(),
+                          )
+                        : Material(
+                            elevation: 1,
+                            shadowColor: Color(0xffb8b7ce),
+                            borderRadius: BorderRadius.circular(10),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Color(0xffececf8),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: TextField(
+                                controller: searchTEController,
+                                onChanged: (value) {
+                                  initialSearch(value.toUpperCase());
+                                },
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  prefixIcon: Icon(Icons.search),
+                                  hint: Text("Search Username..."),
+                                ),
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                    ),
                     const SizedBox(height: 20),
                     Material(
                       elevation: 1,
@@ -191,7 +222,10 @@ class _HomeScreenState extends State<HomeScreen> {
                       child: Container(
                         padding: EdgeInsets.all(10),
                         width: MediaQuery.of(context).size.width,
-                        decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(10)),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
                         child: Row(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
@@ -204,18 +238,18 @@ class _HomeScreenState extends State<HomeScreen> {
                                 fit: BoxFit.cover,
                               ),
                             ),
-                            const SizedBox(width: 10,),
+                            const SizedBox(width: 10),
                             Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                const SizedBox(height: 10,),
+                                const SizedBox(height: 10),
                                 Text(
                                   "Aminul Islam",
                                   textAlign: TextAlign.center,
                                   style: TextStyle(
                                     color: Colors.black,
                                     fontSize: 18,
-                                    fontWeight: FontWeight.w500 ,
+                                    fontWeight: FontWeight.w500,
                                   ),
                                 ),
                                 Text(
@@ -224,7 +258,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                   style: TextStyle(
                                     color: Color.fromARGB(151, 0, 0, 0),
                                     fontSize: 18,
-                                    fontWeight: FontWeight.w500 ,
+                                    fontWeight: FontWeight.w500,
                                   ),
                                 ),
                               ],
@@ -236,7 +270,7 @@ class _HomeScreenState extends State<HomeScreen> {
                               style: TextStyle(
                                 color: Colors.black,
                                 fontSize: 18,
-                                fontWeight: FontWeight.bold ,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
                           ],
@@ -252,9 +286,27 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
-  Widget buildResultCard(data){
+
+  Widget buildResultCard(data) {
     return GestureDetector(
-      onTap: ()async{},
+      onTap: () async {
+        search = false;
+        var chatRoomId = getChatRoomIdByUserName(myUserName!, data['userName']);
+        Map<String, dynamic> chatInfoMap = {
+          "users": [myUserName, data["userName"]],
+        };
+        await DatabaseMethods().createChatRoom(chatRoomId, chatInfoMap);
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ChatScreen(
+              name: data["Name"],
+              profileUrl: data["Image"],
+              userName: data["userName"],
+            ),
+          ),
+        );
+      },
       child: Container(
         margin: EdgeInsets.symmetric(vertical: 8),
         child: Material(
@@ -264,7 +316,7 @@ class _HomeScreenState extends State<HomeScreen> {
             padding: EdgeInsets.all(18),
             decoration: BoxDecoration(
               color: Colors.white,
-              borderRadius: BorderRadius.circular(10)
+              borderRadius: BorderRadius.circular(10),
             ),
             child: Row(
               children: [
@@ -277,18 +329,18 @@ class _HomeScreenState extends State<HomeScreen> {
                     fit: BoxFit.cover,
                   ),
                 ),
-               const SizedBox(width: 20,),
+                const SizedBox(width: 20),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const SizedBox(height: 10,),
+                    const SizedBox(height: 10),
                     Text(
                       data["Name"],
                       textAlign: TextAlign.center,
                       style: TextStyle(
                         color: Colors.black,
                         fontSize: 18,
-                        fontWeight: FontWeight.w500 ,
+                        fontWeight: FontWeight.w500,
                       ),
                     ),
                     Text(
@@ -297,7 +349,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       style: TextStyle(
                         color: Color.fromARGB(151, 0, 0, 0),
                         fontSize: 18,
-                        fontWeight: FontWeight.w500 ,
+                        fontWeight: FontWeight.w500,
                       ),
                     ),
                   ],
